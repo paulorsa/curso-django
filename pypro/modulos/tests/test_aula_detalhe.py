bc3f4251e4e1ceffbee85962a8f8f3cec588cadd
paulorsa <paulorsa.almeida@gmail.com>
2021-03-17 12:26:17 -0700

    Implementado a listagem de indices. close #16

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -25,5 +25,11 @@ def resp(client, aula):
 def test_titulo(resp, aula: Aula):
     assert_contains(resp, aula.titulo)
 
+
 def test_youtube(resp, aula: Aula):
     assert_contains(resp, f'"https://www.youtube.com/embed/{ aula.youtube_id }"')
+
+
+def test_modulo_breadcrumb(resp, modulo: Modulo):
+    assert_contains(resp, f'<li class="breadcrumb-item"><a href="{ modulo.get_absolute_url() }">{ modulo.titulo }</a></li>')
+
